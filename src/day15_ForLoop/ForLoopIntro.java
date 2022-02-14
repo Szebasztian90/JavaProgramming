d27ee87b6f22ad321893a120cc175e866fb955bc
sebastiandulau <dulausebastian@gmail.com>
2022-02-14 15:29:30 +0000

    Rest of the files

---
+++ src/day15_ForLoop/ForLoopIntro.java
@@ -0,0 +1,35 @@
+package day15_ForLoop;
+
+public class ForLoopIntro {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+
+        System.out.println("------------------------------");
+
+
+        for(int i = 1; i <= 10; i++  ){ // it gets to be executed 10 times, after that it becomes false
+
+            System.out.println("Hello World");
+
+        }
+
+
+
+
+
+
+
+    }
+}
