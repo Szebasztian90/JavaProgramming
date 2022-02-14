6c5e67ca0aaecd058c29b456897c924aaa3e8dd1
sebastiandulau <dulausebastian@gmail.com>
2022-02-14 11:28:38 +0000

    Rest of the files

--- liveReviewSessions/week04/AdaireAparments.java
+++ liveReviewSessions/week04/AdaireAparments.java
@@ -1,5 +1,34 @@
 package week04;
 
 public class AdaireAparments {
+    public static void main(String[] args) {
+
+        System.out.println("######## WELCOME TO ADAIRE APARTMENTS #########");
+
+        int numberOfBedrooms = 1;
+        double startingPrice = 0;
+
+        switch (numberOfBedrooms){
+            case 0:
+                System.out.println("You have selected studio apartment");
+                startingPrice = 1454.0;
+                break;
+            case 1:
+                    System.out.println("You have selected one bedroom apartment");
+                    startingPrice = 1725.0;
+                    break;
+            case 2:
+                System.out.println("You have selected two bedroom apartment");
+                startingPrice = 2899.0;
+                break;
+
+            default:
+                System.out.println(numberOfBedrooms + " bedroom apartment not available");
+
+        }
+
+        System.out.println("Starting prise: $ " + startingPrice);
+
+    }
 
 }
---
+++ src/day15_ForLoop/ReplaceX.java
@@ -0,0 +1,28 @@
+package day15_ForLoop;
+
+public class ReplaceX {
+    public static void main(String[] args) {
+
+
+        String str = "xcodeX";
+
+        String result = str.replace("x", "a").replace("X", "a");
+        // the string return will be lowered x and for X need another replace
+
+
+        System.out.println(result);
+
+
+
+
+
+        /*
+2. Write a program that asks user to enter a word. and replace all the x or X with character a
+                    Input:
+                        xcodeX
+                    Output:
+                        acodea
+ */
+
+    }
+}
