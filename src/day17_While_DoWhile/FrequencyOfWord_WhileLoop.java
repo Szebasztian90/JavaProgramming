9fbdc09d1aaa4726803ede857ae91001d5ec5525
sebastiandulau <dulausebastian@gmail.com>
2022-02-21 15:12:14 +0000

    Rest of the files

--- src/day17_While_DoWhile/FrequencyOfWord_WhileLoop.java
+++ src/day17_While_DoWhile/FrequencyOfWord_WhileLoop.java
@@ -35,11 +35,27 @@ public class FrequencyOfWord_WhileLoop {
         System.out.println(countCat);
 
 
+        System.out.println("---------------------------------------------------------");
 
+        String s = "java java java python python python";
 
+        int countJava = 0;
+        int countPython = 0;
 
+        while(s.contains("java") || s.contains("python")){
 
+            if(s.contains("java")){
+                s = s.replaceFirst("java","");
+                countJava++;
+            }
+            if(s.contains("python")){
+                s = s.replaceFirst("python", "");
+                countPython++;
+            }
+        }
 
+        System.out.println("countJava = " + countJava);
+        System.out.println("countPython = " + countPython);
 
 
     }
