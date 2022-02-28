075f4ef2798d3c93c5f1c2a620234151b43d8502
sebastiandulau <dulausebastian@gmail.com>
2022-02-28 22:15:49 +0000

    Rest of the files

--- src/day20_Arrays/ArraysPractice.java
+++ src/day20_Arrays/ArraysPractice.java
@@ -2,7 +2,7 @@ package day20_Arrays;
 
 import java.util.Arrays;
 
-public class ArraysPracticeOne {
+public class ArraysPractice {
     public static void main(String[] args) {
 
         // store the elements: 10, 20, 50, 70
