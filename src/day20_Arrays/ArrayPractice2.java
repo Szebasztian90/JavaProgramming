d0bdfa98d12238941eb3c02ebab831694ae42775
sebastiandulau <dulausebastian@gmail.com>
2022-02-28 22:52:00 +0000

    Rest of the files

---
+++ src/day20_Arrays/ArrayPractice2.java
@@ -0,0 +1,40 @@
+package day20_Arrays;
+
+import java.util.Arrays;
+
+public class ArrayPractice2 {
+    public static void main(String[] args) {
+
+        char [] letters = new char [26];
+
+        /*
+        letters [0] = 'A';
+        letters [1] = 'B';
+         */
+
+       /* for(char i='A', j=0; i <='Z' && j< letters.length; i++, j++){ // i: -> represents the index numbers 0~last index
+            letters[j] = i;                                           // j: -> represents the number of counting []
+
+        }
+        */
+        char ch = 'A';
+        for(int i=0; i < letters.length; i++){
+            letters[i]=ch++;
+        }
+
+        System.out.println(Arrays.toString(letters)); // [A~Z]
+
+
+        System.out.println("---------------------------------------");
+
+        char [] letters2 = new char[26];
+
+        for (char i = 'Z', j=0; i >= 'A' && j < letters2.length; i--, j++) {
+            letters2[j] = i;
+
+        }
+        System.out.println(Arrays.toString(letters2));
+
+
+    }
+}
