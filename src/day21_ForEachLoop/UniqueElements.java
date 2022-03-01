f7a6c8824041751ed4514e20c71cd28bb5a2f624
sebastiandulau <dulausebastian@gmail.com>
2022-03-01 22:46:55 +0000

    Rest of the files

--- src/day21_ForEachLoop/UniqueElements.java
+++ src/day21_ForEachLoop/UniqueElements.java
@@ -6,7 +6,8 @@ public class UniqueElements {
         String [] words = {"Layan", "Layan", "Oleksandr", "Olga", "Adam", "Adam", "Adam", "Cihad", "Cihad", "Cydeo"};
 
 
-        for (String each : words) {
+        for (String each : words) { // each: {"Layan", "Layan", "Oleksandr", "Olga", "Adam", "Adam", "Adam",
+                                    // "Cihad", "Cihad", "Cydeo"};
             int count = 0;
 
             for (String element : words) {
