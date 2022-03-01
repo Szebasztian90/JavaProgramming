9c8448eb97311566fdb9dcae6460771cd4e86272
sebastiandulau <dulausebastian@gmail.com>
2022-03-01 23:32:08 +0000

    Rest of the files

--- src/day21_ForEachLoop/StringMethods.java
+++ src/day21_ForEachLoop/StringMethods.java
@@ -1,6 +1,7 @@
 package day21_ForEachLoop;
 
 import java.util.Arrays;
+import java.util.SortedMap;
 
 public class StringMethods {
     public static void main(String[] args) {
@@ -15,7 +16,29 @@ public class StringMethods {
         }
 
 
+        System.out.println("---------------------------------------------------------");
 
+        String sentence = "Wooden Spoon";
+
+       String[] words = sentence.split(" ");
+
+        System.out.println(Arrays.toString(words));
+
+        System.out.println("--------------------------------------------------------------");
+
+        String email = "WoodenSpoon@Cydeo.com";
+
+        String[] arr = email.split("@");
+
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("-----------------------------------------------------------");
+
+        String s = "Today is a nice day. Today is Monday. Today we learn Java";
+
+        String[] sentences = s.split("\\. ");
+
+        System.out.println(Arrays.toString(sentences));
 
 
 
