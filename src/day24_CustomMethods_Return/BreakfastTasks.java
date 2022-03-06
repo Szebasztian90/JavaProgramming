535b560c1c4dc0d18ae8e1ab571752259205fb9a
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-06 20:37:21 +0000

    Intellij Ultimate

---
+++ src/day24_CustomMethods_Return/BreakfastTasks.java
@@ -0,0 +1,31 @@
+package day24_CustomMethods_Return;
+
+public class BreakfastTasks {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+
+}
+/*
+Warmup tasks:
+	1. Create a method that can display the initials of the person
+	2. Create a method that can display the domain of the email
+	3. Create a method that can display the name of the month based on the given number to the method
+	4. Create a method that can print the name of the day based on the given number to the method
+	5. Create a method that can print how many days a month has
+    6. ageGroups
+                age groups are:
+                        infant (1 - 2), Toddler (3 - 5),Kid (6 - 9), Pre-Teen (10 - 12),
+                        Teenager (13 - 17),Young Adult (18 - 20),Adult (21 - 39),
+                        Young Middle-Aged Adult (40 - 49),Middle-Aged Adult (50 - 54),
+                        Very Young Senior Citizen (55 - 64),Young Senior Citizen (65 - 74),
+                        Senior Citizen (75 - 84),
+                        Old Senior Citizen (85+)
+                        */
