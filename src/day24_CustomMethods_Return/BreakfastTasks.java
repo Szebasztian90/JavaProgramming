66389935f66d32edd16ff7be732c282cb432c943
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-09 11:09:07 +0000

    Intellij Ultimate

--- src/day24_CustomMethods_Return/BreakfastTasks.java
+++ src/day24_CustomMethods_Return/BreakfastTasks.java
@@ -1,12 +1,47 @@
 package day24_CustomMethods_Return;
 
+import java.util.Locale;
+
 public class BreakfastTasks {
     public static void main(String[] args) {
 
+        initials("wooden","Spoon");
+
+        System.out.println("---------------------");
+
+        domain("Cydeo.School@cydeoshool.com");
+
+        System.out.println("-----------------------------------");
+
+        String[] emails = {"josh@gmail.com", "Jim@yahoo.com", "Elminur@cydeo.com", "Gulsen@gmail.com" };
+
+        for (String email : emails) {
+            domain(email);
+        }
+
+        }
 
 
+//1. Create a method that can display the initials of the person. name => initials(String firstName, String lastName)
+
+    public static void initials(String firstName, String lastName){
+        String initial = firstName.charAt(0) + "." + lastName.charAt(0);
+        initial = initial.toUpperCase();
+        System.out.println("initial = " + initial);
     }
-//1. Create a method that can display the initials of the person. initials()
+
+
+   // 2. Create a method that can display the domain of the email. will be called => domain (String email)
+
+   public static void domain(String email){
+
+        String domain = email.substring(email.indexOf("@") +1, email.lastIndexOf("."));
+       System.out.println("domain = " + domain);
+   }
+
+
+
+
 
 
 
