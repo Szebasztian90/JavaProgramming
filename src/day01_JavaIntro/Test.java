453b39b5ef7127fc29785aca68bd61351f45a860
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-09 15:07:35 +0000

    Intellij Ultimate

---
+++ src/day01_JavaIntro/Test.java
@@ -0,0 +1,16 @@
+package day01_JavaIntro;
+
+import day24_CustomMethods_Return.ReturnMethodPractice4;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        String str = "aaaaaaaaaaaaaabbbbbbbbbbbbbbbb";
+
+        str = ReturnMethodPractice4.removeDuplicates(str);
+
+        System.out.println(str);
+
+    }
+}
