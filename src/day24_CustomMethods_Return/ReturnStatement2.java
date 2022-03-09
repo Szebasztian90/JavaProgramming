4ceb7a292b3514d0d8f3c1898babbf90f570dd54
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-09 13:47:01 +0000

    Intellij Ultimate

--- src/day24_CustomMethods_Return/ReturnStatement2.java
+++ src/day24_CustomMethods_Return/ReturnStatement2.java
@@ -13,7 +13,7 @@ public class ReturnStatement2 {
 
         if (number < 1 || number >12){
             System.out.println("Invalid");
-            return; // exits nameOfMonth method
+            return; // exits nameOfMonth method, remaining code fragments of the method never gets executed
         }
 
         String name = (number == 1) ? "Jan" : (number == 2) ? "Feb" : (number == 3) ? "Mar" : (number == 4) ? "Apr" : (number == 5) ? "May"
