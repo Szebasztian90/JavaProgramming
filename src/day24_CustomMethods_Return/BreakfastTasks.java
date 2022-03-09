8bd820e6df8d4a21571fcf49ff36212302af8f93
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-09 11:45:14 +0000

    Intellij Ultimate

--- src/day24_CustomMethods_Return/BreakfastTasks.java
+++ src/day24_CustomMethods_Return/BreakfastTasks.java
@@ -19,10 +19,17 @@ public class BreakfastTasks {
             domain(email);
         }
 
-        }
+        System.out.println("---------------------------------------");
+
+        nameOfMonth(6);
 
 
-//1. Create a method that can display the initials of the person. name => initials(String firstName, String lastName)
+    }
+
+
+
+
+    //1. Create a method that can display the initials of the person. name => initials(String firstName, String lastName)
 
     public static void initials(String firstName, String lastName){
         String initial = firstName.charAt(0) + "." + lastName.charAt(0);
@@ -40,12 +47,39 @@ public class BreakfastTasks {
    }
 
 
+   //3. Create a method that can display the name of the month based on the given number to the method
+
+    public static void nameOfMonth(int number){
+        String name = "";
+
+        if(number >=1 && number <=12){
+
+            name = (number==1)? "Jan" :(number==2)? "Feb" :(number==3)? "March" :(number==4)? "April"
+                    :(number==5) ?"May" :(number==6)? "June" :(number==7)? "July" :(number==8)? "August"
+                    :(number==9)? "Sept" :(number==10)? "October" :(number==11)?"Nov" :"Dec";
+
+        }else{
+            name = "Invalid";
+        }
+
+        System.out.println("Month name = " + name);
+
+    }
 
 
+    //4. Create a method that can print the name of the day based on the given number to the method
 
+    public static void nameOfDay(int number){
 
+    }
 
+    // 5. Create a method that can print how many days a month has
 
+    public static void daysInMonth(int number){
+
+    }
+
+    // 6. ageGroups
 
 }
 /*
