b217de38e53dca7df7132cbd1b00388872ba4867
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-10 11:30:33 +0000

    Intellij Ultimate

--- src/day01_JavaIntro/Test.java
+++ src/day01_JavaIntro/Test.java
@@ -3,6 +3,8 @@ package day01_JavaIntro;
 import day24_CustomMethods_Return.ReturnMethodIntro;
 import day24_CustomMethods_Return.ReturnMethodPractice4;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -19,6 +21,44 @@ public class Test {
        String reversedName = ReturnMethodIntro.reverse(name);
 
         System.out.println(reversedName);
+
+        System.out.println("--------------------------");
+
+        int[] intArray = new int[10];
+        for (int i = 0; i <= intArray.length-1; i++) {
+            if( i % 2 ==0){
+                intArray[i]= i;
+
+            }else{
+                intArray[i] =i;
+            }
+            System.out.println(Arrays.toString(intArray));
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
