bc0b409401478364d292e9aec6ffe54e2a4e7e24
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-14 16:44:29 +0000

    IntelliJ Ultimate

--- src/day01_JavaIntro/Test.java
+++ src/day01_JavaIntro/Test.java
@@ -1,65 +1,13 @@
 package day01_JavaIntro;
 
-import day24_CustomMethods_Return.ReturnMethodIntro;
-import day24_CustomMethods_Return.ReturnMethodPractice4;
-
-import java.util.Arrays;
+import utilities.StringUtility;
 
 public class Test {
     public static void main(String[] args) {
 
+        String str = "Wooden Spoon";
 
-        String str = "aaaaaaaaaaaaaabbbbbbbbbbbbbbbb";
-
-        str = ReturnMethodPractice4.removeDuplicates(str);
-
-        System.out.println(str);
-
-
-        String name = "Java Programming";
-
-       String reversedName = ReturnMethodIntro.reverse(name);
-
-        System.out.println(reversedName);
-
-        System.out.println("--------------------------");
-
-        int[] intArray = new int[10];
-        for (int i = 0; i <= intArray.length-1; i++) {
-            if( i % 2 ==0){
-                intArray[i]= i;
-
-            }else{
-                intArray[i] =i;
-            }
-            System.out.println(Arrays.toString(intArray));
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        StringUtility.printEachChar(str);
 
     }
 }
---
+++ src/utilities/StringUtility.java
@@ -0,0 +1,16 @@
+package utilities;
+
+public class StringUtility {
+
+    // prints each character of the given String
+    public static void printEachChar(String str){
+
+        for (int i = 0; i <str.length() ; i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+
+
+
+}
+
