e262ad578fcea4dbc32c4d54123c549fbdb30b00
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-14 18:13:45 +0000

    IntelliJ Ultimate

---
+++ src/utilities/ArraysUtility.java
@@ -0,0 +1,41 @@
+package utilities;
+
+public class ArraysUtility {
+
+    // prints each integer of an integer array in separate lines.
+    public static void printEach(int[] array){
+        for (int each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+    // prints each DOUBLE of the double array in separate lines.
+    public static void printEach(double[] array){
+        for (double each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+    // prints each CHAR of the char array in separate lines
+    public static void printEach(char[] array){
+        for (char each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+    //prints each STRING of the string array in separate lines
+    public static void printEach(String[] array){
+        for (String each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+
+}
+
+
+
