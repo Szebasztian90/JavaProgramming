ea04054bde9f4fd6d083b95c8a8f400919fbe0ff
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-14 16:07:02 +0000

    IntelliJ Ultimate

---
+++ src/day25_CustomMethod_Overloading/MainMethodOverloading.java
@@ -0,0 +1,21 @@
+package day25_CustomMethod_Overloading;
+
+public class MainMethodOverloading {
+    public static void main(String[] args) {
+        System.out.println("String");
+    }
+
+    public static void main(int [] args) {
+        System.out.println("Int");
+    }
+
+    public static void main(double[] args) {
+        System.out.println("Double");
+    }
+
+    public static void main(char[] args) {
+        System.out.println("Char");
+    }
+
+
+}
