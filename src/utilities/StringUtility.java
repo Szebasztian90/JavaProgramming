a2c5125a98b4648d97db71b6787e1d60fc5a37dc
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-14 17:15:24 +0000

    IntelliJ Ultimate

--- src/utilities/StringUtility.java
+++ src/utilities/StringUtility.java
@@ -2,7 +2,7 @@ package utilities;
 
 public class StringUtility {
 
-    // prints each character of the given String
+    // prints EACH CHARACTER of the given String
     public static void printEachChar(String str){
 
         for (int i = 0; i <str.length() ; i++) {
@@ -10,6 +10,15 @@ public class StringUtility {
         }
     }
 
+    // REVERSE the given string returns the reversed String
+    public static String reverse(String str){
+        String result = "";
+
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
 
 
 }
