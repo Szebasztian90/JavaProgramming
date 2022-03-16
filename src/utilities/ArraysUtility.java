844c76ea7bb0a08c68c77f36a6c1bbdcd2cd8d9d
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-16 19:01:13 +0000

    IntelliJ Ultimate

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import java.util.Arrays;
+
 public class ArraysUtility {
 
     // prints each integer of an integer array in separate lines.
@@ -34,6 +36,74 @@ public class ArraysUtility {
     }
 
 
+    // find the MAXIMUM number from integer array
+    public static int max(int[] numbers){
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1];
+    }
+
+
+
+    // find the MAXIMUM number from double array
+    public static double max(double[] numbers){
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1];
+    }
+
+
+    // find the MINIMUM number from integer array
+    public static int min(int[] numbers){
+        Arrays.sort(numbers);
+        return numbers[0];
+    }
+
+
+    // find the MINIMUM number from double array
+    public static double min(double[] numbers){
+        Arrays.sort(numbers);
+        return numbers[0];
+    }
+
+
+    // check if the given INTEGER is contained in the given array. returns boolean. contains (int[], int)
+    public static boolean contains(int[] array, int element){
+        // {1,2,3,4}      find if 6 is contained in the array
+        boolean result = false;
+
+        for (int each : array) {
+            if(each == element){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    // check if the given DOUBLE is contained in the given array. returns boolean.
+    public static boolean contains(double[] array, double element){
+
+        boolean result = false;
+
+        for (double each : array) {
+            if(each == element){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
+    //check if the given CHAR is contained in the given array. returns boolean.
+    public static boolean contains(char[] array, char element){
+
+        boolean result = false;
+
+        for (char each : array) {
+            if(each == element){
+                result = true;
+            }
+        }
+        return result;
+    }
 
 }
 
