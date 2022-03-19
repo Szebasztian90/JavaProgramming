6cde2f586b7825c21d067e4b710fa38af33eb355
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-19 14:48:32 +0000

    IntelliJ Ultimate

---
+++ liveReviewSessions/week10/R03_CalculateRetirement.java
@@ -0,0 +1,27 @@
+package week10;
+
+public class R03_CalculateRetirement {
+
+    // This example was implemented to show that we can call a method from other methods
+
+    public static void main(String[] args) {
+
+
+        yearsUntilRetirement("Oscar",1977);
+    }
+
+
+    // calculate age of that person
+
+    public static int calculateAge(int yearOfBirth){
+        return 2022-yearOfBirth;
+    }
+
+    //let's assume 65 is out retirement age
+
+    public static void yearsUntilRetirement(String name, int yearOfBirth){
+        int yearsUntilRetirement = 65 - calculateAge(yearOfBirth);
+        System.out.println(name + " retires in " + yearsUntilRetirement + " years");
+    }
+
+}
