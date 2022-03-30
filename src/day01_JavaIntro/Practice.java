9d3d7f14672235f435269ca56f0d890893b7026e
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-30 20:24:49 +0100

    IntelliJ Ultimate

---
+++ src/day01_JavaIntro/Practice.java
@@ -0,0 +1,36 @@
+package day01_JavaIntro;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
