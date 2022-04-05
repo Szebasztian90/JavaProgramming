32c033a9b6d6e8bae21b601981f51c2379453ae0
Sebastian Dulau <dulausebastian@gmail.com>
2022-04-05 11:11:03 +0100

    IntelliJ Ultimate

---
+++ src/day36_Inheritance/Encapsulation/StudentObject.java
@@ -0,0 +1,11 @@
+package day36_Inheritance.Encapsulation;
+
+public class StudentObject {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Daniel", 26, 'M', 'A', "Cydeo");
+
+        student1.setAge(32);
+        System.out.println(student1);
+    }
+}
---
+++ src/day36_Inheritance/animalTask/Dog.java
@@ -0,0 +1,11 @@
+package day36_Inheritance.animalTask;
+
+public class Dog extends Animals { //Dog is an animal
+//           Child        Parent
+
+    public void bark(){
+        System.out.println(name + " is barking.");
+    }
+
+
+}
