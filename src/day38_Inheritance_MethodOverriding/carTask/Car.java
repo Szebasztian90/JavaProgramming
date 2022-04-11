7185e9dc39348d0a2f5624264b2dd4557279b222
Sebastian Dulau <dulausebastian@gmail.com>
2022-04-11 12:08:32 +0100

    Rest of the files

---
+++ src/day38_Inheritance_MethodOverriding/carTask/Car.java
@@ -0,0 +1,52 @@
+package day38_Inheritance_MethodOverriding.carTask;
+
+public class Car {
+
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println(brand + " " + model + " is starting.");
+    }
+
+    public void drive(){
+        System.out.println(brand + " " + model + " is driving.");
+    }
+
+   // public void fly(){ is not common for all the cars, and parent class should only contain the common feature of all the subclass
+   // }
+
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+
+
+}
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+				add a constructor to set all the fields
+			instance methods:
+				start(), drive(),toString()
+ */
