cef442b3d1bfa991a40909c8d90b0b7f5fe31705
Sebastian Dulau <dulausebastian@gmail.com>
2022-04-19 17:55:31 +0100

    Rest of the files

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,21 @@
+package day41_Exceptions;
+
+public class Checked_vs_Unchecked {
+    public static void main(String[] args) {
+
+        //unchecked exception:
+
+        int a= 10;
+        int b = 0;
+
+       // System.out.println( a / b );
+
+        char[] characters = {'A', 'B', 'C'};
+
+        //System.out.println(characters[99]);
+
+
+
+
+    }
+}
